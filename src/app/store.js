6494adcd9d162a10ceabaaed2bb1a586b4fe21c7
Nic Last <niclast7611@gmail.com>
2022-08-03 13:44:42 -0700

    slice, reducer created, imported into store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import altCounterReducer from '../features/altcounter/altCounterSlice'
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    altCounter: altCounterReducer
   },
 });
