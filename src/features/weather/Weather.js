acba0e62dc93028245c17b05292305ef2bfa5b55
Damien Richcreek <damien@richcreek.dev>
2022-08-05 15:07:38 -0700

    refactored weather api

--- src/features/weather/Weather.js
+++ src/features/weather/Weather.js
@@ -1,9 +1,50 @@
 import React from 'react'
 
 const Weather = () => {
+<<<<<<< Updated upstream
     return (
         <h1>Weather</h1>
+=======
+    const [weatherData, setWeatherData] = useState()
+
+    useEffect(() => {
+        async function fetchData() {
+            await fetch(`https://api.openweathermap.org/data/2.5/weather?%7Bq%7D=&%7Bappid%7D=&q=San%20Diego&appid=${process.env.REACT_APP_API_KEY}`)
+                .then(response => response.json())
+                .then((payload) => setWeatherData(payload))
+                .catch(err => console.error(err));
+        }
+        fetchData()
+    }, [])
+
+    const kelvinToFarenheight = tempInKelvin => {
+        return Math.round((tempInKelvin - 273.15) * 9 / 5 + 32)
+    }
+
+    return (
+        <div id="weather">
+            {
+                weatherData === undefined ?
+                    (<div>Loading</div>) :
+                    (
+                        <>
+                            <h1>Weather</h1>
+                            <div>
+                                <h2>{weatherData.name} Weather</h2>
+                                <ul>
+                                    <li>Current Temp: {kelvinToFarenheight(weatherData.main.temp)} &#8457;</li>
+                                    <li>Current Humidity: {weatherData.main.humidity}%</li>
+                                </ul>
+                            </div>
+                        </>
+
+                    )
+            }
+
+        </div>
+>>>>>>> Stashed changes
     )
 }
 
 export default Weather
+
