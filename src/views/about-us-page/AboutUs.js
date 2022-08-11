9df305a15a02ef5bb8ad3881c95ea5e9c50a1109
Damien Richcreek <damien@richcreek.dev>
2022-08-11 16:50:57 -0700

    Updated slide-out nav

--- src/views/about-us-page/AboutUs.js
+++ src/views/about-us-page/AboutUs.js
@@ -6,108 +6,109 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 export default function AboutUs() {
-  return (
-    <div id ="aboutus" className='flex-wrap' >
-        {/* damien card */}
-        <container spacing={2}>
-        <Grid xs={6}>
-        <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-            component="img"
-            height="140"
-            image="https://avatars.githubusercontent.com/u/16322338?v=4"
-            alt="Mr. Richcreek"
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            Mr. Richcreek
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-            I'm a Full Stack Software Engineer building professional web applications!
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">GitHub</Button>
-            <Button size="small">LinkedIn</Button>
-            <Button size="small">Portfolio</Button>
-        </CardActions>
-        </Card>
-        </Grid>
-        <Grid>
-        {/* frank card */}
-        <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-            component="img"
-            height="140"
-            image="https://avatars.githubusercontent.com/u/98143232?v=4"
-            alt="Mr. Rivera"
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            Mr. Rivera
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-            I'm a Junior Full Stack Developer building professional web applications
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">GitHub</Button>
-            <Button size="small">LinkedIn</Button>
-            <Button size="small">Portfolio</Button>
-        </CardActions>
-        </Card>
-        </Grid>
-        <Grid>
-        {/* nic card */}
-        <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-            component="img"
-            height="140"
-            image="https://avatars.githubusercontent.com/u/104329673?v=4"
-            alt="Mr. Last"
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            Mr. Last
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-            I'm a Junior Full Stack Developer building professional web applications
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">GitHub</Button>
-            <Button size="small">LinkedIn</Button>
-            <Button size="small">Portfolio</Button>
-        </CardActions>
-        </Card>
-        </Grid>
-        <Grid>
-        {/* val card */}
-        <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-            component="img"
-            height="140"
-            image="https://avatars.githubusercontent.com/u/104322947?v=4"
-            alt="Mrs. Tovar"
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            Mrs. Tovar
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-            I'm a Junior Full Stack Developer building professional web applications
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">GitHub</Button>
-            <Button size="small">LinkedIn</Button>
-            <Button size="small">Portfolio</Button>
-        </CardActions>
-        </Card>
-        </Grid>
-        </container>
-    </div>
-  );
+    return (
+        <div id="aboutus" className='flex-wrap' >
+            {/* damien card */}
+            <Container spacing={2}>
+                <Grid item={true} xs={6}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="https://avatars.githubusercontent.com/u/16322338?v=4"
+                            alt="Mr. Richcreek"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Mr. Richcreek
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                I'm a Full Stack Software Engineer building professional web applications!
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">GitHub</Button>
+                            <Button size="small">LinkedIn</Button>
+                            <Button size="small">Portfolio</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid>
+                    {/* frank card */}
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="https://avatars.githubusercontent.com/u/98143232?v=4"
+                            alt="Mr. Rivera"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Mr. Rivera
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                I'm a Junior Full Stack Developer building professional web applications
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">GitHub</Button>
+                            <Button size="small">LinkedIn</Button>
+                            <Button size="small">Portfolio</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid>
+                    {/* nic card */}
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="https://avatars.githubusercontent.com/u/104329673?v=4"
+                            alt="Mr. Last"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Mr. Last
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                I'm a Junior Full Stack Developer building professional web applications
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">GitHub</Button>
+                            <Button size="small">LinkedIn</Button>
+                            <Button size="small">Portfolio</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid>
+                    {/* val card */}
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="https://avatars.githubusercontent.com/u/104322947?v=4"
+                            alt="Mrs. Tovar"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Mrs. Tovar
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                I'm a Junior Full Stack Developer building professional web applications
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">GitHub</Button>
+                            <Button size="small">LinkedIn</Button>
+                            <Button size="small">Portfolio</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
