3b8aead75e0e298ae863f5ca64f0ccdb4d1462e6
Damien Richcreek <damien@richcreek.dev>
2022-08-15 15:44:32 -0700

    removed extra router import

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
